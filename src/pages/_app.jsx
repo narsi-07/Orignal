cf9a16314098b3716e2539ffaad458a37c2b4182
narsi-07 <dazzlecode1@gmail.com>
2024-07-15 09:19:38 +0530

    delete title

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -3,13 +3,13 @@ import { Provider as ConnectionProvider } from 'context/connect'
 import { useRouter } from 'next/router'
 import Sidebar from 'components/Sidebar';
 import Head from 'next/head';
-import { mainConfig } from 'config/config';
+
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
   return <ConnectionProvider>
     <Head>
-      <title>Dazzlone | Chat with random people!</title>
+   
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
     {router.pathname.includes('rooms') ? (
