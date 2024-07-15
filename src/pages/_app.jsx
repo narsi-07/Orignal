6552e5882725a8bf975b3f05b7bb269fa22e75c5
narsi-07 <dazzlecode1@gmail.com>
2024-07-15 09:15:00 +0530

    title

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -9,7 +9,7 @@ function MyApp({ Component, pageProps }) {
   const router = useRouter();
   return <ConnectionProvider>
     <Head>
-      <title>{mainConfig.nameApp} | Chat with random people!</title>
+      <title>Dazzlone | Chat with random people!</title>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
     {router.pathname.includes('rooms') ? (
